222e1b7be530c12faf37c31efd3352a201dc04cd
dp221125 <dp221125@naver.com>
2023-08-11 21:25:18 +0900

    Update Package

---
+++ Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "GPGoogleMaps-SPM",
+    products: [
+        .library(
+            name: "GPGoogleMaps-SPM",
+            targets: [
+                "GoogleMaps",
+                "GoogleMapsBase",
+                "GoogleMapsCore"
+            ]),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "GoogleMaps",
+            url: "https://github.com/good-potatos/GPGoogleMaps-SPM/releases/download/8.0.0/GoogleMaps.xcframework.zip",
+            checksum: "5492e272ab47205e1d48c3828229ccd474e9677786c87895452c740a91860156"
+        ),
+        .binaryTarget(
+            name: "GoogleMapsBase",
+            url: "https://github.com/good-potatos/GPGoogleMaps-SPM/releases/download/8.0.0/GoogleMapsBase.xcframework.zip",
+            checksum: "5859f0846861e3d1b44f5bd0ce9ea1cc156368b89f07b0445918fda235f66402"
+        ),
+        .binaryTarget(
+            name: "GoogleMapsCore",
+            url: "https://github.com/good-potatos/GPGoogleMaps-SPM/releases/download/8.0.0/GoogleMapsCore.xcframework.zip",
+            checksum: "a2f3f325da97815da382394b0d08411ccdee0310c9f5a210a6b00e9d3ca22882"
+        )
+    ]
+)
